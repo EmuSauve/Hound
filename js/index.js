51bc6b0030ff223b7cd1a83f5f86ac4f12cbf5b2
EmuSauve <em.kiuchi@gmail.com>
2022-08-04 13:25:36 +0900

    Edited text

--- js/index.js
+++ js/index.js
@@ -25,3 +25,26 @@ window.onresize = function(e) {
         menuLine.removeClass('active');
     }
 };
+
+// GoToTop
+$(function(){
+
+    /* TOP-Pageボタン制御 */
+    var topBtn = $('.goto-top-wrapper');
+    topBtn.hide();
+    //スクロールが100に達したらボタン表示
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            topBtn.fadeIn();
+        } else {
+            topBtn.fadeOut();
+        }
+    });
+    // //スクロールしてトップ
+    // topBtn.click(function () {
+    //     $('body,html').animate({
+    //         scrollTop: 0
+    //     }, 500);
+    //     return false;
+    // });
+});
